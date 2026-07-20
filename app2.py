7eb6d7202a29f247b47af2d66333150b1cc96bcc
Huhdayeon <yeonni0717@gmail.com>
2026-07-20 13:51:23 +0900

    Update app2.py

--- app2.py
+++ app2.py
@@ -30,9 +30,9 @@ with tab1:
     st.info("너무 튀는 신발은 지양하도록 해요!")
 with tab2:
   st.write("포인트 아이템:")
-  acc = st.selectbox("액세서리 추가", ["모자", "안경", "목걸이", "가방"])
+  acc = st.multiselect("액세서리 추가", ["모자", "안경", "목걸이", "가방"])
   with st.expander("액세서리 스타일링 팁 보기 "):
-    st.info("너무 많은 액세서리는 투머치가 될 수 있어요!")
+    st.warning("너무 많은 액세서리는 투머치가 될 수 있어요!")
 
 st.markdown("---")
 if st.button("코디 완성하기"):
@@ -42,7 +42,7 @@ if st.button("코디 완성하기"):
     st.markdown(f"""
     * **상의:** {top_color} {top_type}
     * **하의:** {bottom_color} {bottom_type}
-    * **매칭:** {shoes}와 {', '.join(acc) if acc else '악세서리 없이 깔끔하게!'}
+    * **매칭:** {shoes}와 {','.join(acc) if acc else '악세서리 없이 깔끔하게!'}
     """)
     st.success("오늘의 스타일링이 완성되었습니다. 자신있게 외출하세요!")
   with st.expander("코디 연출 팁 영상 보기"):
