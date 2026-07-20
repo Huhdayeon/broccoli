33a1cac6f43962407e463127d55c3c21a8a37b14
Huhdayeon <yeonni0717@gmail.com>
2026-07-20 11:19:50 +0900

    Change grade input to radio and update ban default

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import streamlit as st
 
 st.markdown("# 앱 UI 만들기")
 user_id = st.text_input("이름", placeholder="이름")
-grade = st.checkbox(["1", "2", "3"])
-ban = st.number_input("반", min_value=1, max_value=11, value=3)
+grade = st.radio("학년", ["1", "2", "3"], horizontal=True)
+ban = st.number_input("반", min_value=1, max_value=11, value=1)
 hard = st.select_slider("난이도",options=["쉬움", "보통", "어려움", "매우 어려움"],value="보통")
 score = st.slider("점수", 0, 100, 50)
 feel = st.text_area("소감", placeholder="소감합니다.")
