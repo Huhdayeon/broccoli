f39dcce5ee85a8532ab1f61d2e016f95f43dac5a
Huhdayeon <yeonni0717@gmail.com>
2026-07-20 15:03:50 +0900

    Update app4.py

--- app4.py
+++ app4.py
@@ -18,15 +18,13 @@ with col1:
       st.session_state.start_time = time.time() # 현재 시각 기록
       st.session_state.end_time = 0          # 종료 시간 초기화
 with col2:
-  if st.session_state.end_time == 0:
     if st.button("종료"):
-        if st.session_state.start_time != 0:
+        if st.session_state.end_time == 0:
+          if st.session_state.start_time != 0:
           st.session_state.end_time = time.time()
           st.session_state.result = st.session_state.end_time - st.session_state.start_time
         else:
             st.warning("시작 버튼을 먼저 눌러주세요!")
-  else:
-    st.warning("이미 게임이 끝났습니다. 다시하기를 눌러주세요.")
 
 if st.session_state.end_time != 0:
     diff = st.session_state.result
