51af97511b374dad6ca0c87545bff02bdfcd50d6
Huhdayeon <yeonni0717@gmail.com>
2026-07-21 10:41:27 +0900

    Update OpenAI API key retrieval method

--- app5.py
+++ app5.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 from openai import OpenAI
-ai_client = OpenAI(api_key=st.secrets["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"])
+ai_client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 if 'todo_list' not in st.session_state:
     st.session_state.todo_list = []
 if 'user_motto' not in st.session_state:
